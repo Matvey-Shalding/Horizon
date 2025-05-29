2eed3405eff62b4bf257637efe0083a9249b5c49
Mathew Shaldin <mathew.shaldin@gmail.com>
2025-05-29 08:42:30 +0300

    style: improve checkbox ui

--- src/ui/Checkbox.tsx
+++ src/ui/Checkbox.tsx
@@ -1,75 +1,70 @@
 "use client";
 
-import { motion } from "framer-motion";
+import clsx from "clsx";
+import { useState } from "react";
 import { UseFormRegister } from "react-hook-form";
 
-interface AnimatedCheckboxProps {
+interface CheckboxProps {
   register?: UseFormRegister<any>;
   name?: string;
   styles?: string;
-  svgStyle?: string;
   style?: React.CSSProperties;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   [key: string]: any;
 }
 
 export const Checkbox = ({
-  svgStyle,
-  styles = "",
   register,
   name,
+  styles = "",
   style,
   onChange,
   ...props
-}: AnimatedCheckboxProps) => {
-  return (
-    <div className="flex items-center gap-2">
-      <label className="group relative flex cursor-pointer items-center">
-        <input
-          type="checkbox"
-          {...(register
-            ? register(String(name), {
-                required: "You must accept the privacy policy",
-              })
-            : {})}
-          className="peer hidden outline-none"
-          {...props}
-          onChange={onChange}
-        />
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(false);
 
-        <motion.div
-          className={`relative flex h-5 w-5 items-center justify-center rounded-main border border-gray-300 bg-white shadow-sm transition-all duration-300 peer-checked:border-blue-600 peer-checked:bg-light-blue peer-hover:ring-2 peer-hover:ring-blue-300 ${styles}`}
-          whileTap={{ scale: 0.95 }}
-          whileHover={{ scale: 1.03 }}
-          transition={{
-            type: "spring",
-            stiffness: 250,
-            damping: 20,
-          }}
+  return (
+    <label
+      className={clsx("relative inline-flex items-center", styles)}
+      style={style}
+    >
+      <input
+        type="checkbox"
+        className="peer absolute h-0 w-0 opacity-0"
+        {...(register && register(String(name)))}
+
+        
+        onChange={(e) => {
+          setChecked(e.target.checked);
+          onChange?.(e);
+        }}
+        {...props}
+      />
+
+      <span
+        className={clsx(
+          `border-border n mt-0.5 flex h-4 w-4 items-center justify-center rounded-[4.5px] border-1 border-solid`,
+          "border-border bg-white transition-all duration-300 ease-in-out",
+          "peer-hover:border-light-blue",
+          "peer-checked:bg-light-blue peer-checked:border-none",
+          "peer-checked:scale-105 peer-active:scale-95",
+        )}
+      >
+        <svg
+          className={clsx(
+            "h-3.5 w-3.5 transition-all duration-300 ease-in-out",
+            checked ? "scale-100 opacity-100" : "scale-50 opacity-0",
+          )}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="white"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
-          <motion.svg
-            initial={{ pathLength: 0, opacity: 0 }}
-            animate={{
-              pathLength: 1,
-              opacity: 1,
-            }}
-            exit={{ pathLength: 0, opacity: 0 }}
-            transition={{
-              duration: 0.35,
-              ease: "easeInOut",
-            }}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="white"
-            strokeWidth="3"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className={`h-3 w-3 ${svgStyle}`}
-          >
-            <polyline points="20 6 9 17 4 12" />
-          </motion.svg>
-        </motion.div>
-      </label>
-    </div>
+          <polyline points="20 6 9 17 4 12" />
+        </svg>
+      </span>
+    </label>
   );
 };
