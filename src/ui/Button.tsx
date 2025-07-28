3eb239531d9c737d34cd20079c3d3e445aaa661e
Mathew Shaldin <mathew.shaldin@gmail.com>
2025-07-28 21:05:46 +0300

    style: add responsive styling for buttons

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,39 +1,46 @@
 "use client";
 
-import { motion } from "framer-motion"; // Import framer-motion
-
+import { motion } from "framer-motion";
+import { useMediaQuery } from "@react-hookz/web";
+import { MEDIA_QUERIES } from 'settings/MediaQueries';
 export function Button({
   content,
   props,
   styles,
-  onClick
+  onClick,
 }: {
   content: string;
   props?: any;
   styles?: string;
   onClick?: () => void;
 }) {
+  const isMobile = useMediaQuery(`(max-width: ${MEDIA_QUERIES.MOBILE})`);
+
   return (
     <motion.button
       {...props}
       onClick={onClick}
-      className={` ${String(styles)} rounded-main gradient min-h-11 w-full text-base/normal text-white disabled:cursor-not-allowed`}
-      initial={{ scale: 1, boxShadow: "0 0 0 rgba(0, 0, 0, 0)" }} // Initial state
-      whileHover={{
-        scale: 1.1, // Increase scale more on hover for emphasis
-        boxShadow: "0 8px 30px rgba(0, 0, 0, 0.2)", // Larger and more intense shadow on hover
-        backgroundColor: "#ff5500", // More vibrant hover background color
-        transition: { duration: 0.3 }, // Smooth and quick transition on hover
-      }}
+      className={`rounded-main gradient min-h-11 w-full text-base/normal text-white disabled:cursor-not-allowed ${styles || ""}`}
+      initial={{ scale: 1 }}
+      whileHover={
+        !isMobile
+          ? {
+              scale: 1.1,
+              boxShadow: "0 8px 30px rgba(0, 0, 0, 0.2)",
+              backgroundColor: "#ff5500",
+              transition: { duration: 0.3 },
+            }
+          : {}
+      }
       whileTap={{
-        scale: 0.95, // Slightly scale down on click
-        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)", // Less intense shadow on tap
+        scale: 0.95,
+        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)",
       }}
       transition={{
         type: "spring",
         stiffness: 300,
         damping: 25,
-        duration: 0.3, // Smooth transition for scale and shadow
+        duration: 0.3,
       }}
     >
       {content}
