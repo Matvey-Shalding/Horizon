ba34e8a122a788d233635543f82583631333ec79
Mathew Shaldin <mathew.shaldin@gmail.com>
2025-08-01 06:49:34 +0300

    perf: optimize menuItems data

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -3,31 +3,17 @@ import Card from 'components/icons/sidebar/nav/Card';
 import Home from 'components/icons/sidebar/nav/Home';
 import Transaction from 'components/icons/sidebar/nav/Transactions';
 import Transfer from 'components/icons/sidebar/nav/Transfer';
-import type { ComponentType, SVGProps } from 'react';
 import { MAIN_ROUTES } from 'routes';
 import MenuItem from 'types/MenuItem.interface';
 
-
-
 /**
  * Sidebar menu configuration.
+ * @constant
  */
 export const menuItems: MenuItem[] = [
   { icon: Home, label: 'Home', path: MAIN_ROUTES.HOME },
-  { icon: Banks, label: 'My banks', path: MAIN_ROUTES.BANKS },
-  {
-    icon: Transaction,
-    label: 'Transaction History',
-    path: MAIN_ROUTES.TRANSACTIONS,
-  },
-  {
-    icon: Transfer,
-    label: 'Payment Transfer',
-    path: MAIN_ROUTES.TRANSFER,
-  },
-  {
-    icon: Card,
-    label: 'Connect Bank',
-    path: MAIN_ROUTES.CONNECT_BANK,
-  },
+  { icon: Banks, label: 'My Banks', path: MAIN_ROUTES.BANKS },
+  { icon: Transaction, label: 'Transaction History', path: MAIN_ROUTES.TRANSACTIONS },
+  { icon: Transfer, label: 'Payment Transfer', path: MAIN_ROUTES.TRANSFER },
+  { icon: Card, label: 'Connect Bank', path: MAIN_ROUTES.CONNECT_BANK },
 ];
