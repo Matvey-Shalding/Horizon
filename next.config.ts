48e7cf84c84c577162a05a29173b36a1e36d4ff6
Mathew Shaldin <mathew.shaldin@gmail.com>
2025-08-06 16:18:13 +0300

    fix: trying to deploy

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: 'Matvey-Shalding/Horizon',
+  basePath: '/Matvey-Shalding/Horizon',
 
   /**
    * Disable server-based image optimization. Next.js does not support
