11d5b012d6f2ed0feb384d0a7fd4f16d4bd54d39
Mathew Shaldin <mathew.shaldin@gmail.com>
2025-08-07 07:31:10 +0300

    fix: temporary disable middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,44 @@
-import { getToken } from 'next-auth/jwt';
-import { NextRequest, NextResponse } from 'next/server';
-import { AUTH_ROUTES } from 'routes';
-import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT } from '../routes';
+// import { getToken } from 'next-auth/jwt';
+// import { NextRequest, NextResponse } from 'next/server';
+// import { AUTH_ROUTES } from 'routes';
+// import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT } from '../routes';
 
-export async function middleware(req: NextRequest) {
-  const { nextUrl } = req;
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+// export async function middleware(req: NextRequest) {
+//   const { nextUrl } = req;
+//   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  const isLoggedIn = !!token;
+//   const isLoggedIn = !!token;
 
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname as any);
+//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+//   const isAuthRoute = authRoutes.includes(nextUrl.pathname as any);
 
-  // Allow access to API authentication routes
-  if (isApiAuthRoute) {
-    return NextResponse.next();
-  }
+//   // Allow access to API authentication routes
+//   if (isApiAuthRoute) {
+//     return NextResponse.next();
+//   }
 
-  // Redirect authenticated users visiting "/" to a default route (e.g., /dashboard)
-  if (isLoggedIn && nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-  }
+//   // Redirect authenticated users visiting "/" to a default route (e.g., /dashboard)
+//   if (isLoggedIn && nextUrl.pathname === '/') {
+//     return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+//   }
 
-  // Handle authentication routes
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
-    return NextResponse.next();
-  }
+//   // Handle authentication routes
+//   if (isAuthRoute) {
+//     if (isLoggedIn) {
+//       return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+//     }
+//     return NextResponse.next();
+//   }
 
-  if (!isLoggedIn) {
-    return NextResponse.redirect(new URL(AUTH_ROUTES.LOGIN, nextUrl));
-  }
+//   if (!isLoggedIn) {
+//     return NextResponse.redirect(new URL(AUTH_ROUTES.LOGIN, nextUrl));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-  ],
-};
+// export const config = {
+//   matcher: [
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//   ],
+// };
