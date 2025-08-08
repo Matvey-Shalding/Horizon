8ac8aed537df1cb264a9f2bfcbc5ffd158ac18c2
Mathew Shaldin <mathew.shaldin@gmail.com>
2025-08-08 10:32:15 +0300

    fix bug with middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,51 +4,51 @@ import { AUTH_ROUTES } from 'routes';
 import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT } from '../routes';
 
 export async function middleware(req: NextRequest) {
-  try {
-    const { nextUrl } = req;
+  // try {
+  //   const { nextUrl } = req;
 
-    console.log('Next url', nextUrl);
+  //   console.log('Next url', nextUrl);
 
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  //   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-    console.log('secret code', process.env.NEXTAUTH_SECRET);
+  //   console.log('secret code', process.env.NEXTAUTH_SECRET);
 
-    console.log('token', token);
+  //   console.log('token', token);
 
-    const isLoggedIn = !!token;
+  //   const isLoggedIn = !!token;
 
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname as any);
+  //   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  //   const isAuthRoute = authRoutes.includes(nextUrl.pathname as any);
 
-    console.log('Is api', isApiAuthRoute);
+  //   console.log('Is api', isApiAuthRoute);
 
-    console.log('Is auth', isAuthRoute);
+  //   console.log('Is auth', isAuthRoute);
 
-    if (isApiAuthRoute) {
-      return NextResponse.next();
-    }
+  //   if (isApiAuthRoute) {
+  //     return NextResponse.next();
+  //   }
 
-    if (isLoggedIn && nextUrl.pathname === '/') {
-      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
+  //   if (isLoggedIn && nextUrl.pathname === '/') {
+  //     return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  //   }
 
-    if (isAuthRoute) {
-      if (isLoggedIn) {
-        return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-      }
-      return NextResponse.next();
-    }
+  //   if (isAuthRoute) {
+  //     if (isLoggedIn) {
+  //       return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  //     }
+  //     return NextResponse.next();
+  //   }
 
-    if (!isLoggedIn) {
-      return NextResponse.redirect(new URL(AUTH_ROUTES.LOGIN, nextUrl));
-    }
+  //   if (!isLoggedIn) {
+  //     return NextResponse.redirect(new URL(AUTH_ROUTES.LOGIN, nextUrl));
+  //   }
 
-    return NextResponse.next();
-  } catch (error) {
-    console.error('Middleware error:', error);
-    // Allow request to proceed even if there is an error
-    return NextResponse.next();
-  }
+  //   return NextResponse.next();
+  // } catch (error) {
+  //   console.error('Middleware error:', error);
+  //   // Allow request to proceed even if there is an error
+  //   return NextResponse.next();
+  // }
 }
 
 export const config = {
